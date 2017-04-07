c40c81192922ea48dc835da8f7a75798444134b3
OlafvdGraaf <0928885@hr.nl>
2017-04-07 14:27:29 +0200

    search button made in java file

--- homescreen_project_app/app/src/main/java/com/example/dymos/homescreen_project_app/homescreen.java
+++ homescreen_project_app/app/src/main/java/com/example/dymos/homescreen_project_app/homescreen.java
@@ -1,19 +1,34 @@
 package com.example.dymos.homescreen_project_app;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Timer;
+
 
 public class homescreen extends AppCompatActivity {
 
-    Button searchBtn;
+    Button exitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
 
-        searchBtn = (Button) findViewById(R.id.searchBtn);
+        exitBtn = (Button) findViewById(R.id.exitBtn);
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
 
+            }
+        });
     }
 }
