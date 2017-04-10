0a7bd1d62461665f6e79f9ec843f4f97aa15e463
Yori <0903577@hr.nl>
2017-04-10 14:51:30 +0200

    random bar graphs //TODO: gegevens ophalen.

--- homescreen_project_app/app/src/main/java/com/example/dymos/homescreen_project_app/search_results.java
+++ homescreen_project_app/app/src/main/java/com/example/dymos/homescreen_project_app/search_results.java
@@ -10,37 +10,42 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class search_results extends AppCompatActivity {
 
     BarChart barChart;
+    Random rand = new Random();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String value1;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
 
         BarChart barChart = (BarChart) findViewById(R.id.BarGraph1);
 
         ArrayList<BarEntry> barEntries = new ArrayList();
-        for(int i = 0; i < 10; i++){
-            i = i+1;
+        ArrayList<String> theDates = new ArrayList<>();
+
+        for(int i = 0; i < 7; i++){
+            float n = rand.nextInt(50);
+
+            barEntries.add(new BarEntry(n,i));
+            String region = Float.toString(i);
+            theDates.add("Regio" + region);
         }
-        barEntries.add(new BarEntry(44f, 0));
+
+        /*barEntries.add(new BarEntry(44f, 0));
         barEntries.add(new BarEntry(23f, 1));
         barEntries.add(new BarEntry(38f, 2));
         barEntries.add(new BarEntry(34f, 3));
         barEntries.add(new BarEntry(14f, 4));
-        barEntries.add(new BarEntry(24f, 5));
+        barEntries.add(new BarEntry(24f, 5));*/
 
         BarDataSet barDataSet = new BarDataSet(barEntries,"Dates");
 
-        ArrayList<String> theDates = new ArrayList<>();
-        theDates.add("January");
-        theDates.add("Febraury");
-        theDates.add("March");
-        theDates.add("April");
-        theDates.add("May");
-        theDates.add("June");
+
 
         BarData theData = new BarData(theDates,barDataSet);
         barChart.setData(theData);
