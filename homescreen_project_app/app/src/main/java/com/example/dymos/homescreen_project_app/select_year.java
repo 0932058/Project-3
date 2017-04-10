93e5f775ce7ca815c2c2cfaf92e624dea11667bc
OlafvdGraaf <0928885@hr.nl>
2017-04-10 10:02:55 +0200

    added new activities

--- homescreen_project_app/app/src/main/java/com/example/dymos/homescreen_project_app/select_year.java
+++ homescreen_project_app/app/src/main/java/com/example/dymos/homescreen_project_app/select_year.java
@@ -1,19 +1,13 @@
 package com.example.dymos.homescreen_project_app;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-public class aboutScreen extends AppCompatActivity {
-
+public class select_year extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_screen);
-
-        setTitle("De Rotterdam criminaliteit app");
-
+        setContentView(R.layout.activity_select_year);
     }
 }
