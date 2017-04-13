39079f50009702dc2226401fbfa027e9dabb8fc3
OlafvdGraaf <0928885@hr.nl>
2017-04-13 10:50:45 +0200

    added: new page with drawable folder

--- homescreen_project_app/app/src/main/java/com/example/dymos/homescreen_project_app/confirm_selection.java
+++ homescreen_project_app/app/src/main/java/com/example/dymos/homescreen_project_app/confirm_selection.java
@@ -1,22 +1,39 @@
 package com.example.dymos.homescreen_project_app;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class confirm_selection extends AppCompatActivity {
 
     Button confirmBtn;
+    TextView region1chosen;
+    TextView region2chosen;
+    ImageView imageRegion1;
+    ImageView imageRegion2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_selection);
+        /*
 
 
+        region1chosen = (TextView) findViewById(R.id.region1chosenTxt);
+        region1chosen.setText(homescreen.selecting.getRegion1());
 
+        region2chosen = (TextView) findViewById(R.id.region2chosenTxt);
+        region2chosen.setText(homescreen.selecting.getRegion2());
+
+        imageRegion1 = (ImageView) findViewById(R.id.imageRegion1);
+        imageRegion1.setImageResource(storeView);
+
+        */
         confirmBtn = (Button) findViewById(R.id.confirmBtn);
         confirmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- homescreen_project_app/app/src/main/java/com/example/dymos/homescreen_project_app/selectRegion.java
+++ homescreen_project_app/app/src/main/java/com/example/dymos/homescreen_project_app/selectRegion.java
@@ -7,7 +7,11 @@ package com.example.dymos.homescreen_project_app;
 public class selectRegion {
 
     private String region1;
+    private int region1Image;
+
     private String region2;
+    private int region2Image;
+
     private int year;
 
     public selectRegion() {
@@ -16,7 +20,26 @@ public class selectRegion {
         year = 0;
     }
 
-    public boolean addRegion(String region){
+    public String getRegion1(){
+        return region1;
+    }
+    public int getRegion1Image(){
+        return region1Image;
+    }
+
+
+    public String getRegion2(){
+        return region2;
+    }
+    public int getRegion2Image(){
+        return region2Image;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public boolean addRegion(String region, int image){
         if (region1.equals(region) || region2.equals(region)){
             this.removeRegion(region);
             return false;
@@ -24,11 +47,13 @@ public class selectRegion {
 
         if (region1.equals("")){
             region1 = region;
+            region1Image = image;
             return true;
         }
 
         if (region2.equals("")){
             region2 = region;
+            region2Image = image;
             return true;
         }
 
@@ -38,10 +63,12 @@ public class selectRegion {
     private void removeRegion(String region){
         if (region1.equals(region)){
             region1 = "";
+            region1Image = 0;
         }
 
         if (region2.equals(region)){
             region2 = "";
+            region2Image = 0;
         }
 
     }
