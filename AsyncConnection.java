ccb3b6fe8bd16995d08d98271db8a24c19f0b1d8
Lucas de Graaf <0932058@hr.nl>
2017-04-19 09:40:35 +0200

    Added attempt connection database
    Fully commented files to prevent further errors

---
+++ AsyncConnection.java
@@ -0,0 +1,31 @@
+package com.test.lucas.servertest;
+
+import android.os.AsyncTask;
+import android.widget.Button;
+
+import junit.framework.Test;
+
+import java.sql.Connection;
+
+/**
+ * Created by lucas on 18-4-2017.
+ */
+//
+//public class AsyncConnection extends AsyncTask<Void, Void, Connection> {
+//
+//    public Connection con;
+//
+//    protected Connection doInBackground(Void...params) {
+//        con = ConnectionConfiguration.getConnection();
+//        return con;
+//    }
+//
+//    protected void onPostExecute(Connection conn) {
+//        if (con != null) {
+//            DataHandler.setText("NIET NULLLLLLLL");
+//        }
+//        else {
+//            DataHandler.setText("jammer dan");
+//        }
+//    }
+//}
---
+++ ConnectionConfiguration.java
@@ -0,0 +1,35 @@
+package com.test.lucas.servertest;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Created by lucas on 18-4-2017.
+ */
+
+//public class ConnectionConfiguration  {
+//
+//    public static Connection getConnection(){
+//        Connection connection = null;
+//
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Log.d("connection", "we are here");
+//            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/CrimeRotterdam", "rootroot", "password");
+//
+//
+//        }
+//        catch (Exception e) {
+//            Log.d("hiero", e.getMessage());
+//            Log.d("hiero", e.toString());
+//            e.printStackTrace();
+//        }
+//
+//        return connection;
+//    }
+//}
+
+
